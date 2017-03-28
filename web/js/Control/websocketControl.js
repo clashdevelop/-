d1c6b7fdb3333080909ad7eeae10fa04703179b7
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2017-03-28 14:43:48 +0800

    马丹github

--- web/js/Control/websocketControl.js
+++ web/js/Control/websocketControl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var websocket;
 //新接受消息
 
@@ -20,12 +19,4 @@ websocket.onerror = function(){
 }
 websocket.onclose = function(){
 	sendMessage("Close");
-=======
-var ws;
-var linkString = "ws://115.159.212.28:8888/";
-ws = new WebSocket(linkString,"123");
-
-ws.onmessage() = function(evt){
-	console.log(evt.data);
->>>>>>> origin/master
 }
